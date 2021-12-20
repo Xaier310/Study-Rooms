1c199cb2be3f9c3d0fca49acce6d7af1335c0677
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2021-12-20 14:30:12 +0530

    final build4

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const io = new Server(server, {
   },
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
