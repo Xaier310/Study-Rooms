a6426da52ce0f7554a5d4332a9273b9de1922113
Amit Batra <75168062+amitbatra31@users.noreply.github.com>
2021-12-20 14:19:58 +0530

    final build

--- index.js
+++ index.js
@@ -36,25 +36,25 @@ function randomStr() {
   return ans;
 }
 
-
-
-
-
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000", "https://admin.socket.io/"],
     methods: ["GET", "POST"],
-  }
+  },
 });
 
-
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 instrument(io, {
   auth: false,
 });
-server.listen(3001, () => {
+server.listen(PORT, () => {
   try {
-    console.log("Backend server is running on port 3001");
+    console.log(`Backend server is running on port ${PORT}`);
   } catch (err) {
     console.log(err);
   }
@@ -74,19 +74,18 @@ io.on("connection", (socket) => {
   var rooms = io.sockets.adapter.rooms;
   io.in(socket.id).socketsLeave(socket.id);
   io.emit("welcome", "Hello this is a chat app");
-  
+
   socket.on("addUser", (username) => {
     addUser(username, socket.id);
   });
-
 
   socket.on("join_room", async (obj) => {
     // console.log("objjjj : backend : ",obj);
     if (obj.type === "oldRoom") {
       if (rooms.has(obj.roomId)) {
-        if(!rooms.get(obj.roomId).has(socket.io)){
+        if (!rooms.get(obj.roomId).has(socket.io)) {
           await socket.join(obj.roomId);
-           socket.roomId = obj.roomId;
+          socket.roomId = obj.roomId;
         }
         socket.emit("isJoined", true);
       } else {
@@ -95,13 +94,13 @@ io.on("connection", (socket) => {
       }
     } else if (obj.type === "newRoom") {
       if (rooms.has(obj.roomId)) {
-      if(!rooms.get(obj.roomId).has(socket.id)){
-        obj.roomId = randomStr();
-        await socket.join(obj.roomId);
-        socket.roomId = obj.roomId;
-        socket.emit("isJoined", true);
-      }}
-      else{
+        if (!rooms.get(obj.roomId).has(socket.id)) {
+          obj.roomId = randomStr();
+          await socket.join(obj.roomId);
+          socket.roomId = obj.roomId;
+          socket.emit("isJoined", true);
+        }
+      } else {
         await socket.join(obj.roomId);
         socket.roomId = obj.roomId;
         socket.emit("isJoined", true);
@@ -110,75 +109,69 @@ io.on("connection", (socket) => {
     // console.log("rooms : ", rooms);
   });
 
-
-socket.on("join_custom_room",async (roomId)=>{
-  if (rooms.has(roomId)) {
-    if(!rooms.get(roomId).has(socket.id)){
-      await socket.join(roomId);
-      socket.roomId = roomId;
-      socket.emit("isJoined", true);
-    }}
-    else{
+  socket.on("join_custom_room", async (roomId) => {
+    if (rooms.has(roomId)) {
+      if (!rooms.get(roomId).has(socket.id)) {
+        await socket.join(roomId);
+        socket.roomId = roomId;
+        socket.emit("isJoined", true);
+      }
+    } else {
       await socket.join(roomId);
       socket.roomId = roomId;
       socket.emit("isJoined", true);
     }
-})
-
+  });
 
   socket.on("send_message", (msg) => {
     // console.log("send chlra hai");
     socket.to(msg.roomId).emit("receive_message", msg);
   });
 
-
-  socket.on("custom_stats",(roomStr)=>{
+  socket.on("custom_stats", (roomStr) => {
     var length = [];
-    for(let i=0;i<5 ;i++){
-      if(rooms.get(`${roomStr}${i+1}`))
-       length.push(rooms.get(`${roomStr}${i+1}`).size);
+    for (let i = 0; i < 5; i++) {
+      if (rooms.get(`${roomStr}${i + 1}`))
+        length.push(rooms.get(`${roomStr}${i + 1}`).size);
       else length.push(0);
     }
-    socket.emit("your_custom_stats",length);
-  })
+    socket.emit("your_custom_stats", length);
+  });
 
-socket.on("give_roomUsers",(roomId)=>{
-  var arr=[];
-  if(rooms.has(roomId)){
-    arr = Array.from(rooms.get(roomId));
-  }
-  console.log(roomId,arr.length);
-  io.in(roomId).emit("get_roomUsers",arr);
-})
-
-socket.on("remove_me",(roomId)=>{
-if(roomId){
-  if(rooms.has(roomId)){
-    if(rooms.get(roomId).has(socket.id)){
-      io.in(roomId).socketsLeave(socket.id);
+  socket.on("give_roomUsers", (roomId) => {
+    var arr = [];
+    if (rooms.has(roomId)) {
+      arr = Array.from(rooms.get(roomId));
     }
-}
-removeUser(socket.id);
-console.log("erf ",socket.roomId);
-if(socket.roomId){
-  console.log("gfhj : ",users.length);
-  socket.to(socket.roomId).emit("getUsers", users);
-}
-}
-})
+    console.log(roomId, arr.length);
+    io.in(roomId).emit("get_roomUsers", arr);
+  });
 
-socket.on("giveUsers",(roomId)=>{
-  socket.to(roomId).emit("getUsers",users);
-  console.log("Users connected : ",users.length);
-})
+  socket.on("remove_me", (roomId) => {
+    if (roomId) {
+      if (rooms.has(roomId)) {
+        if (rooms.get(roomId).has(socket.id)) {
+          io.in(roomId).socketsLeave(socket.id);
+        }
+      }
+      removeUser(socket.id);
+      console.log("erf ", socket.roomId);
+      if (socket.roomId) {
+        console.log("gfhj : ", users.length);
+        socket.to(socket.roomId).emit("getUsers", users);
+      }
+    }
+  });
+
+  socket.on("giveUsers", (roomId) => {
+    socket.to(roomId).emit("getUsers", users);
+    console.log("Users connected : ", users.length);
+  });
 
   socket.on("disconnect", () => {
     removeUser(socket.id);
-    if(socket.roomId){
+    if (socket.roomId) {
       socket.to(socket.roomId).emit("getUsers", users);
     }
   });
 });
-
-
-
